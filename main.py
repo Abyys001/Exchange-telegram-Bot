916401491accf113576d14cdee71322dbcb710fa
Siavash drv <119043855+Abyys001@users.noreply.github.com>
2025-08-17 20:45:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ from pyromod import Client
 from plugins.secret_keys import PARDIS_SECRET_KEY
 
 # BOT_TOKEN = PARDIS_SECRET_KEY
-API_ID = 25659111
-API_HASH = "2f4d5e01c109e278ac7d29e907647db1"
 
 PLUGIN = dict(root="plugins")
 
 
 app = Client(
     "app",
-    API_ID,
+    API_ID, # your api and hash XD
     API_HASH,
     plugins=PLUGIN,  # check the plugins folder
     bot_token=PARDIS_SECRET_KEY,
